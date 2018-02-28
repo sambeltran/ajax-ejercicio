a7bba61a638dad86567211c9a74dfffcf869efaa
sambeltran <sambeltranesco20@gmail.com>
2018-02-28 09:43:08 -0500

    upload some upgrades

---
+++ js/file1.js
@@ -0,0 +1,37 @@
+/*===========================
+principios basicos de json. json es una tecnica 
+de enviar y recibir data, esta normalmente formada de
+arrays que contienen objetos.
+
+Java
+Script
+Object
+Notation
+
+jemplo.
+
+
+creamos un array y dentro de el ponemos
+obejtos o las propiedades de algun objeto:
+
+var lasMascotas=[
+	{
+		"name":"gaton",
+		"especie":"gato",
+		"favComida":"atun"
+
+	},
+
+	{
+		"name":"perruno",
+		"especie":"perro",
+		"favComida":"comida para perro"
+	}
+]
+
+para acceder a alguna propiedasd podemos poner:
+ lasMascotas[0].especie
+
+===========================*/
+
+console.log("hola mundo")
